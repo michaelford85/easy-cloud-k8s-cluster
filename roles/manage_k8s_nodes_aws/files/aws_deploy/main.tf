963297df4ab3eacadc50daeaf7bcb55535cee68f
Michael Ford <michaelford85@gmail.com>
2026-02-16 12:54:06 -0600

    adjusted AWS installation

--- roles/manage_k8s_nodes_aws/files/aws_deploy/main.tf
+++ roles/manage_k8s_nodes_aws/files/aws_deploy/main.tf
@@ -1,90 +1,126 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 6.0.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 4.0.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = ">= 2.0.0"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = ">= 3.0.0"
+    }
+  }
+}
+
 provider "aws" {
-  region     = var.ec2_region
-  shared_credentials_file = var.aws_key
-  profile                 = var.aws_profile
+  region                   = var.ec2_region
+  shared_credentials_files = [var.aws_key]
+  profile                  = var.aws_profile
+}
+
+# Discover the public IP of the machine running terraform/ansible and restrict admin access to it.
+data "http" "my_ip" {
+  url = "https://api.ipify.org"
+}
+
+locals {
+  ansible_ip = "${chomp(data.http.my_ip.response_body)}/32"
 }
 
 resource "aws_vpc" "k8s-vpc" {
-  cidr_block = "192.168.0.0/24"
+  cidr_block = var.ec2_vpc_cidr
 
   tags = {
     application  = "kubeadm-k8s"
     cluster-name = var.ec2_prefix
-    Name = "${var.ec2_prefix}-vpc"
+    Name         = "${var.ec2_prefix}-vpc"
   }
 }
 
-
-
-
 resource "aws_security_group" "k8s-master-sg" {
-  name        = "${var.ec2_prefix}-master-sg"
-  vpc_id      = aws_vpc.k8s-vpc.id
+  name   = "${var.ec2_prefix}-master-sg"
+  vpc_id = aws_vpc.k8s-vpc.id
 
+  # Admin access (only from Ansible control machine)
   ingress {
-    description = "kube-apiserver"
-    from_port   = 6443
-    to_port     = 6443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "etcd"
-    from_port   = 2379
-    to_port     = 2380
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "kubelet"
-    from_port   = 10250
-    to_port     = 10250
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "kube-scheduler"
-    from_port   = 10251
-    to_port     = 10251
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "kube-controller-manager"
-    from_port   = 10252
-    to_port     = 10252
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "SSH"
+    description = "SSH from Ansible control machine"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [local.ansible_ip]
   }
 
   ingress {
-    description = "NodePort Services"
-    from_port   = 30000
-    to_port     = 32767
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  
-  ingress {
-    description = "Allow all ICMP"
+    description = "ICMP from Ansible control machine"
     from_port   = -1
     to_port     = -1
     protocol    = "icmp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [local.ansible_ip]
   }
 
+  # Kubernetes API access:
+  # - from Ansible control machine for kubeadm/kubectl during bootstrap
+  ingress {
+    description = "kube-apiserver from Ansible control machine"
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = [local.ansible_ip]
+  }
+
+  # Control-plane internal ports (keep tight)
+  ingress {
+    description = "etcd (self only)"
+    from_port   = 2379
+    to_port     = 2380
+    protocol    = "tcp"
+    self        = true
+  }
+
+  ingress {
+    description = "kubelet on master (self only)"
+    from_port   = 10250
+    to_port     = 10250
+    protocol    = "tcp"
+    self        = true
+  }
+
+  ingress {
+    description = "kube-scheduler (self only)"
+    from_port   = 10259
+    to_port     = 10259
+    protocol    = "tcp"
+    self        = true
+  }
+
+  ingress {
+    description = "kube-controller-manager (self only)"
+    from_port   = 10257
+    to_port     = 10257
+    protocol    = "tcp"
+    self        = true
+  }
+
+  # Allow all traffic within the master SG (covers loopback + any future multi-master expansion if you later
+  # attach additional control-plane nodes to this SG).
+  ingress {
+    description = "Allow all internal traffic within master SG"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    self        = true
+  }
+
+  # Egress should be open so nodes can reach package repos, container registries, etc.
   egress {
     from_port   = 0
     to_port     = 0
@@ -95,13 +131,30 @@ resource "aws_security_group" "k8s-master-sg" {
   tags = {
     application  = "kubeadm-k8s"
     cluster-name = var.ec2_prefix
-    Name = "${var.ec2_prefix}-master-sg"
+    Name         = "${var.ec2_prefix}-master-sg"
   }
 }
 
 resource "aws_security_group" "k8s-worker-sg" {
-  name        = "${var.ec2_prefix}-worker-sg"
-  vpc_id      = aws_vpc.k8s-vpc.id
+  name   = "${var.ec2_prefix}-worker-sg"
+  vpc_id = aws_vpc.k8s-vpc.id
+
+  # Admin access (only from Ansible control machine)
+  ingress {
+    description = "SSH from Ansible control machine"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [local.ansible_ip]
+  }
+
+  ingress {
+    description = "ICMP from Ansible control machine"
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = [local.ansible_ip]
+  }
 
   ingress {
     description = "kubelet"
@@ -111,6 +164,7 @@ resource "aws_security_group" "k8s-worker-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # NodePort Services (lab-friendly). Tighten to [local.ansible_ip] if you want it private.
   ingress {
     description = "NodePort Services"
     from_port   = 30000
@@ -119,38 +173,16 @@ resource "aws_security_group" "k8s-worker-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Allow all traffic within the worker SG (covers worker-to-worker)
   ingress {
-    description = "Weave CNI TCP"
-    from_port   = 6783
-    to_port     = 6784
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all internal traffic within worker SG"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    self        = true
   }
 
-  ingress {
-    description = "Weave CNI UDP"
-    from_port   = 6783
-    to_port     = 6784
-    protocol    = "udp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "Allow all ICMP"
-    from_port   = -1
-    to_port     = -1
-    protocol    = "icmp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
+  # Egress should be open so nodes can reach package repos, container registries, etc.
   egress {
     from_port   = 0
     to_port     = 0
@@ -161,19 +193,64 @@ resource "aws_security_group" "k8s-worker-sg" {
   tags = {
     application  = "kubeadm-k8s"
     cluster-name = var.ec2_prefix
-    Name = "${var.ec2_prefix}-worker-sg"
+    Name         = "${var.ec2_prefix}-worker-sg"
   }
 }
 
+# --- Cross-SG cluster rules (defined as separate resources to avoid Terraform dependency cycles) ---
+
+# Workers -> API server on master
+resource "aws_security_group_rule" "master_api_from_workers" {
+  type                     = "ingress"
+  description              = "kube-apiserver from workers"
+  from_port                = 6443
+  to_port                  = 6443
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.k8s-master-sg.id
+  source_security_group_id = aws_security_group.k8s-worker-sg.id
+}
+
+# Master -> kubelet on workers
+resource "aws_security_group_rule" "worker_kubelet_from_master" {
+  type                     = "ingress"
+  description              = "kubelet from control plane"
+  from_port                = 10250
+  to_port                  = 10250
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.k8s-worker-sg.id
+  source_security_group_id = aws_security_group.k8s-master-sg.id
+}
+
+# Allow all traffic master <-> workers for cluster networking (Calico, overlays, node-to-node, etc.)
+resource "aws_security_group_rule" "master_all_from_workers" {
+  type                     = "ingress"
+  description              = "Allow all internal cluster traffic from workers to master"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = "-1"
+  security_group_id        = aws_security_group.k8s-master-sg.id
+  source_security_group_id = aws_security_group.k8s-worker-sg.id
+}
+
+resource "aws_security_group_rule" "workers_all_from_master" {
+  type                     = "ingress"
+  description              = "Allow all internal cluster traffic from master to workers"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = "-1"
+  security_group_id        = aws_security_group.k8s-worker-sg.id
+  source_security_group_id = aws_security_group.k8s-master-sg.id
+}
+
 resource "aws_subnet" "k8s-subnet" {
-  vpc_id            = aws_vpc.k8s-vpc.id
-  cidr_block        = "192.168.0.0/28"
-  map_public_ip_on_launch = "true"
+  vpc_id                  = aws_vpc.k8s-vpc.id
+  cidr_block              = var.ec2_subnet_cidr
+  map_public_ip_on_launch = true
 
   tags = {
     cluster-name = var.ec2_prefix
-    application = "kubeadm-k8s"
-    Name = "${var.ec2_prefix}-subnet"
+    application  = "kubeadm-k8s"
+    Name         = "${var.ec2_prefix}-subnet"
   }
 }
 
@@ -181,24 +258,24 @@ resource "aws_internet_gateway" "k8s-igw" {
   vpc_id = aws_vpc.k8s-vpc.id
 
   tags = {
-  cluster-name = var.ec2_prefix
-  application = "kubeadm-k8s"
-  Name = "${var.ec2_prefix}-igw"
+    cluster-name = var.ec2_prefix
+    application  = "kubeadm-k8s"
+    Name         = "${var.ec2_prefix}-igw"
   }
 }
 
 resource "aws_default_route_table" "k8s-route-table" {
   default_route_table_id = aws_vpc.k8s-vpc.default_route_table_id
-  route {
-    cidr_block                = "0.0.0.0/0"
-    gateway_id                = aws_internet_gateway.k8s-igw.id
 
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.k8s-igw.id
   }
 
   tags = {
     cluster-name = var.ec2_prefix
-    application = "kubeadm-k8s"
-    Name = "${var.ec2_prefix}-route-table"
+    application  = "kubeadm-k8s"
+    Name         = "${var.ec2_prefix}-route-table"
   }
 }
 
@@ -218,48 +295,56 @@ resource "aws_instance" "k8s-worker-nodes" {
   instance_type = var.machine_type
   key_name      = aws_key_pair.k8s-key.key_name
   subnet_id     = aws_subnet.k8s-subnet.id
-  associate_public_ip_address = "true"
+
+  associate_public_ip_address = true
+
   root_block_device {
     volume_size = var.cloud_worker_volume_size
   }
+
   vpc_security_group_ids = [aws_security_group.k8s-worker-sg.id]
+
   tags = {
-    Name = "${var.ec2_prefix}-worker-${count.index + 1}"
-    cluster-name = var.ec2_prefix
-    application = "kubeadm-k8s"
+    Name           = "${var.ec2_prefix}-worker-${count.index + 1}"
+    cluster-name   = var.ec2_prefix
+    application    = "kubeadm-k8s"
     cloud_provider = "aws"
-    role = "node"
+    role           = "node"
   }
 }
 
 resource "aws_instance" "k8s-master-node" {
   count         = 1
   ami           = var.ec2_image_id
-  instance_type = "t3.xlarge"
+  instance_type = var.master_machine_type
   key_name      = aws_key_pair.k8s-key.key_name
   subnet_id     = aws_subnet.k8s-subnet.id
-  associate_public_ip_address = "true"
+
+  associate_public_ip_address = true
+
   root_block_device {
     volume_size = var.cloud_master_volume_size
   }
+
   vpc_security_group_ids = [aws_security_group.k8s-master-sg.id]
+
   tags = {
-    Name = "${var.ec2_prefix}-master"
-    cluster-name = var.ec2_prefix
-    application = "kubeadm-k8s"
+    Name           = "${var.ec2_prefix}-master"
+    cluster-name   = var.ec2_prefix
+    application    = "kubeadm-k8s"
     cloud_provider = "aws"
-    role = "master"
+    role           = "master"
   }
 }
 
 resource "local_file" "k8s-local-private-key" {
-    content          = tls_private_key.k8s-tls-private-key.private_key_pem
-    filename         = "/tmp/${var.ec2_prefix}-key-private.pem"
-    file_permission  = "0600"
+  content         = tls_private_key.k8s-tls-private-key.private_key_pem
+  filename        = "/tmp/${var.ec2_prefix}-key-private.pem"
+  file_permission = "0600"
 }
 
 resource "local_file" "k8s-local-public-key" {
-    content          = tls_private_key.k8s-tls-private-key.public_key_openssh
-    filename         = "/tmp/${var.ec2_prefix}-key.pub"
-    file_permission  = "0600"
+  content         = tls_private_key.k8s-tls-private-key.public_key_openssh
+  filename        = "/tmp/${var.ec2_prefix}-key.pub"
+  file_permission = "0600"
 }
